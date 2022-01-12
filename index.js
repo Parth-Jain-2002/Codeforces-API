f6315fcf63150da77e96915b8212e46eb9bbdda4
Parth-Jain-2002 <parthljain2002@gmail.com>
2022-01-12 17:06:16 +0530

    Added submission functionality

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ async function getData(){
     console.log(upcoming);
 }
 
-function updateUserInfo(response){
+async function updateUserInfo(x){
+    const url=`https://codeforces.com/api/user.info?handles=${x}`
+    const data = await fetch(url);
+    const response = await data.json();
     document.getElementById("mrating").textContent = response.result[0]["maxRating"]
     document.getElementById("crating").textContent = response.result[0]["rating"]
     document.getElementById("mrank").textContent = response.result[0]["maxRank"]
@@ -21,11 +24,48 @@ function updateUserInfo(response){
     document.getElementById("crank").textContent = document.getElementById("crank").textContent.toUpperCase()
 }
 
-async function SubmitForm(){
-    let x = document.getElementById("userHandle").value;
-    const url=`https://codeforces.com/api/user.info?handles=${x}`
+
+async function updateUserProblem(x,y){
+    const url=`https://codeforces.com/api/user.status?handle=${x}`
     const data = await fetch(url);
     const response = await data.json();
-    updateUserInfo(response)
+    const dat = response.result;
+    const ctime = Math.floor(new Date().getTime()/1000);
+    let AC = 0, WA =0, TLE = 0 , MLE =0 , RE=0;
+    console.log(dat.length);
+    for(let i of dat){
+        if(ctime-i["creationTimeSeconds"]<86400*y){
+            console.log("HI");
+            if(i["verdict"]=="MEMORY_LIMIT_EXCEEDED"){
+                MLE++;
+            }
+            if(i["verdict"]=="TIME_LIMIT_EXCEEDED"){
+                TLE++;
+            }
+            if(i["verdict"]=="RUNTIME_ERROR"){
+                RE++;
+            }
+            if(i["verdict"]=="WRONG_ANSWER"){
+                WA++;
+            }
+            if(i["verdict"]=="OK"){
+                AC++;
+            }
+        }
+    }
+    document.getElementById("AC").textContent = AC
+    document.getElementById("WA").textContent = WA
+    document.getElementById("TLE").textContent = TLE
+    document.getElementById("MLE").textContent = MLE
+    document.getElementById("RE").textContent = RE
 }
+
+async function SubmitForm(){
+    let x = document.getElementById("userHandle").value;
+    let y = document.getElementById("days").value;
+    y = parseInt(y);
+    updateUserProblem(x,y);
+    updateUserInfo(x);
+}
+
 getData();
